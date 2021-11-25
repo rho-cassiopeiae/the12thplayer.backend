7ec8fd7581342cd39c5831f90b72fb99759e36b6
Sergey Che <rho9cas@gmail.com>
2021-11-25 18:11:24 +0800

    Set up Identity.Application
    Add required dependencies and create handle result and error

---
+++ src/Services/Identity/Identity.Application/Common/Errors/HandleError.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace Identity.Application.Common.Errors {
+    public abstract class HandleError {
+        public string Type { get; }
+        public IDictionary<string, string[]> Errors { get; protected set; }
+
+        public HandleError(string type) {
+            Type = type;
+        }
+    }
+}
---
+++ src/Services/Identity/Identity.Application/Common/Results/HandleResult.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.Json.Serialization;
+
+using Identity.Application.Common.Errors;
+
+namespace Identity.Application.Common.Results {
+    public abstract class HandleResult {
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+        public HandleError Error { get; set; }
+    }
+
+    public class HandleResult<T> : HandleResult {
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+        public T Data { get; set; }
+    }
+
+    public class VoidResult : HandleResult<object> {
+        public static readonly VoidResult Instance = new VoidResult();
+    }
+}
