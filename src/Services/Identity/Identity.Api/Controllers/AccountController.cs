c129a11d7b0af20a0c40d779745e093e7e50a30e
Sergey Che <rho9cas@gmail.com>
2021-11-25 17:29:08 +0800

    Initial

---
+++ src/Services/Identity/Identity.Api/Controllers/AccountController.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Identity.Api.Controllers {
+    public class AccountController {
+    }
+}
